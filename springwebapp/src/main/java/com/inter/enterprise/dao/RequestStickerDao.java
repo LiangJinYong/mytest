413d2ccc2d57907c095a398020d1585e2fd2827d
user <user@DESKTOP-6ME2JCP>
2017-10-21 15:42:48 +0900

    필요없는 소스 삭제 (기업업: 스티커 신청, 소비자앱: 예전에 구현했던 로그서비스, DB property파일 -> 프로젝트 외부에서 지정하는 위치에서 DB접속)

--- springwebapp/src/main/java/com/inter/enterprise/dao/RequestStickerDao.java
+++
@@ -1,9 +0,0 @@
-package com.inter.enterprise.dao;
-
-import java.util.Map;
-
-public interface RequestStickerDao {
-
-	void insertRequestSticker(Map<String, String> param);
-
-}
--- springwebapp/src/main/java/com/inter/enterprise/service/RequestStickerService.java
+++
@@ -1,9 +0,0 @@
-package com.inter.enterprise.service;
-
-import java.util.Map;
-
-public interface RequestStickerService {
-
-	String requestSticker(Map<String, String> param);
-
-}
